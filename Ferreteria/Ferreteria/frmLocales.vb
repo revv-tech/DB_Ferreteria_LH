a694a7e21c62a4cb16b020862cadaea78dac9e33
Sebastián <thelopezepol@outlook.es>
2021-05-18 17:32:51 -0600

    Revert "Main"
    This reverts commit f80839d8b29f4d487cf89e47e777ccf3876d13a0.

--- Ferreteria/Ferreteria/frmLocales.vb
+++ Ferreteria/Ferreteria/frmLocales.vb
@@ -2,4 +2,8 @@
     Private Sub frmLocales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CenterToScreen()
     End Sub
+
+    Private Sub SalirButton_Click(sender As Object, e As EventArgs) Handles SalirButton.Click
+        Me.Close()
+    End Sub
 End Class
